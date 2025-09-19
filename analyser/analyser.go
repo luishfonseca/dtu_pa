035626885c9ffd80e10420042676f1076d803026
Luís Fonseca <luis@lhf.pt>
2025-09-19 19:08:37 +0200

    feat: validate arguments for inspect command

--- analyser/analyser.go
+++ analyser/analyser.go
@@ -1,1 +1,24 @@
 package analyser
+
+import "fmt"
+
+type Config struct {
+	ClassFile string
+}
+
+type analyser struct {
+	config Config
+}
+
+func New(classFile string) *analyser {
+	return &analyser{
+		config: Config{
+			ClassFile: classFile,
+		},
+	}
+}
+
+func (a *analyser) Inspect() error {
+	fmt.Printf("Inspecting %s\n", a.config.ClassFile)
+	return nil
+}
--- cmd/inspect.go
+++ cmd/inspect.go
@@ -3,15 +3,36 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/luishfonseca/dtu_pa/analyser"
+
 	"github.com/spf13/cobra"
+
+	"os"
+	"path/filepath"
 )
 
 // inspectCmd represents the inspect command
 var inspectCmd = &cobra.Command{
-	Use:   "inspect",
+	Use:   "inspect [flags] file",
 	Short: "Print the parsed compiled java class in a human readable format",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("inspect called")
+	Args:  cobra.ExactArgs(1),
+	PreRunE: func(_ *cobra.Command, args []string) (err error) {
+		if filepath.Ext(args[0]) != ".class" {
+			return fmt.Errorf("file %s must have a .class extension", args[0])
+		}
+
+		if _, err = os.Stat(args[0]); os.IsNotExist(err) {
+			return fmt.Errorf("file %s does not exist", args[0])
+		}
+
+		if args[0], err = filepath.Abs(args[0]); err != nil {
+			return fmt.Errorf("could not get absolute path of %s: %w", args[0], err)
+		}
+
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return analyser.New(args[0]).Inspect()
 	},
 }
 
