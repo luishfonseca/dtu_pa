fdde08b0eae0dcf1d9634d19aa0140f336cbaf81
Luís Fonseca <luis@lhf.pt>
2025-09-21 19:44:42 +0200

    fix: combine state files

--- state/state.go
+++ state/state.go
@@ -1,5 +1,11 @@
 package state
 
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+)
+
 type fallible interface{ Fail(error) }
 
 type Fn[T fallible] func(T) Fn[T]
@@ -7,5 +13,13 @@ type Fn[T fallible] func(T) Fn[T]
 func Run[T fallible](s T, start Fn[T]) {
 	for state := start; state != nil; {
 		state = state(s)
+	}
+}
+
+func Fail[T fallible](err error) Fn[T] {
+	_, file, line, _ := runtime.Caller(1)
+	return func(s T) Fn[T] {
+		s.Fail(fmt.Errorf("%w (at %s:%d)", err, filepath.Base(file), line))
+		return nil
 	}
 }
